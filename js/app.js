16424d77af9f1a37ad4e91efcee27d107d95e2e0
Sean Wines <sean.wines@vuria.com>
2018-05-19 17:52:35 -0700

    Created landing banner

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(function () {
+  setTimeout(function () {
+    $('.loading-dots').hide();
+    $('#banner .callout').addClass('fade');
+    $('#nav').addClass('show-nav');
+    $('.hero-bg').fadeIn('slow')
+  }, 2000)
+})
